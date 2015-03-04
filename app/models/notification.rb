7a8794b9442c16e7a7bd56811de7574b11986326
Mukhammad Karimov <tafu.karimov@mail.ru>
2015-03-04 19:17:56 +0500

    mend

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,7 @@
 class Notification < ActiveRecord::Base
 	belongs_to :professor
 
-	#has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/default.jpeg"
-	has_attached_file :image, styles: { medium: "300x300>"}
+	has_attached_file :image, styles: { medium: "300x300>",thumb: "100x100>"}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
