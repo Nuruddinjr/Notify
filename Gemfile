0efbd33a94ae89eee2cf3b27a8888661a0b43107
Mukhammad Karimov <tafu.karimov@mail.ru>
2015-03-12 18:12:36 +0500

    Added dropbox

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'byebug'
 gem 'web-console', '~> 2.0'
 gem 'spring'
 
-
+gem "paperclip-dropbox", ">= 1.1.7"
 
 group :development, :test do
 	gem 'sqlite3'
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,7 +5,9 @@ class Notification < ActiveRecord::Base
 	belongs_to :professor
 
 	has_attached_file :image, 
-	styles: { medium: "300x300>",thumb: "100x100>"}
+	styles: { medium: "300x300>",thumb: "100x100>"},
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
